ad44442b6c720e8e1dd7c746ebaf69919e9676b3
Colin Fortuner <cfortuner@gmail.com>
2023-01-14 20:13:39 -0800

    merge

--- src/background/messages/completion.ts
+++ src/background/messages/completion.ts
@@ -3,9 +3,6 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 import { runCompletion } from "~lib/openai"
 
 export const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
-  const compRes = await runCompletion(req.body)
-
-  res.send({
-    text: compRes.choices[0]?.text
-  })
+  const completion = await runCompletion(req.body)
+  res.send(completion)
 }
--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -4,23 +4,29 @@ export const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
 export const runCompletion = async ({
   prompt,
-  config
+  config = {
+    model: "text-davinci-003",
+    max_tokens: 128,
+    temperature: 0.7
+  }
 }: {
   prompt: string
   config: any
 }) => {
   const data = { prompt, ...config, provider: undefined }
   try {
-    const res = await axios("https://api.openai.com/v1/completions", {
+    const res = await fetch("https://api.openai.com/v1/completions", {
       method: "POST",
-      data,
+      body: JSON.stringify(data),
       headers: {
         "Content-Type": "application/json",
         Authorization: "Bearer " + OPENAI_API_KEY
       }
     })
 
-    return res.data
+    const compRes = await res.json()
+
+    return compRes.choices[0].text
   } catch (e) {
     console.error(e)
   }
